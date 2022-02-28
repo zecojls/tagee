77b049e96644cb27545d8d21aba8fa5ec79eead2
s-kganz <ganzkeenan1@gmail.com>
2022-02-28 09:43:36 -0500

    add __all__ parameter

--- tagee/__init__.py
+++ tagee/__init__.py
@@ -1,5 +1,7 @@
 __version__ = "0.1.0"
 
+__all__ = ["terrainAnalysis", "makeVisualization", "logTransformation"]
+
 # Importing these functions makes them directly available to the user as a
 # top-level import (e.g. from tagee import terrainAnalysis). Other functions
 # can be imported as tagee.TAGEE._____
